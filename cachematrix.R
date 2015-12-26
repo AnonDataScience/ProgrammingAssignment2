b5ab9f842d7d581dde90fd45b8b54896c0db691b
AnonDataScience <chris@bongo.co.nz>
2015-12-26 11:52:22 +0000

    Populated makeCacheMatrix and cacheSolve
    Populated the functions to 1) make the matrix object which includes a
    reader and writer for two variables and 2) checks the cache for the
    inverse matrix and if one does not exits, creates one

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: makes an object with methods to read and write values for the original matrix and its inverse
+## cacheSolve: checks if an inverse exists in the object and creates an inverse if not
 
-## Write a short comment describing this function
-
+## creates an object with two variables and four methods: a reader and a writer for each of the two variables
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## checks the cache for the inverse of a matrix and if none exists creates a new one
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
+
